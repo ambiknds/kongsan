e67eb4d1b44a1f7551d339dff5aba14fb8b23c58
ambiknds <ambikndsjobz@gmail.com>
2024-12-07 04:26:16 +0530

    update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -58,7 +58,7 @@ export default function Header() {
             <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div className="flex items-center justify-between">
                 <Link to="/" className="-m-1.5 p-1.5 text-2xl font-bold text-indigo-600">
-                  Christian Resources
+                  Kongsan
                 </Link>
                 <button
                   type="button"
