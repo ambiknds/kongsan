dbbe100d939513bc4c13904feec26efe25e9d510
Ambition Kyndiah <53797801+ambiknds@users.noreply.github.com>
2025-10-11 05:09:19 +0530

    Updated Teachings.jsx

--- src/pages/Teachings.jsx
+++ src/pages/Teachings.jsx
@@ -16,6 +16,7 @@ export default function Teachings() {
           *[_type == "teaching"] | order(publishedAt desc) {
             _id,
             title,
+            
             description,
             content,
             imageUrl,
