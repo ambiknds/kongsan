721ba9a7be910b922ad5e39543fa3a3bee92c66c
Ambition Kyndiah <53797801+ambiknds@users.noreply.github.com>
2025-10-11 05:14:05 +0530

    Updated TeachingDetail.jsx

--- src/pages/TeachingDetail.jsx
+++ src/pages/TeachingDetail.jsx
@@ -12,7 +12,7 @@ export default function TeachingDetail() {
     const fetchTeaching = async () => {
       try {
         const data = await client.fetch(`
-          *[_type == "teaching" && slug.current == "${slug}"][0] {
+          *[_type == "teaching" && slug.current == "${slug}"] {
             _id,
             title,
             slug,
